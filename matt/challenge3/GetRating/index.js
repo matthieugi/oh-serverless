cb19a6df26b4067b4592e60e68b9242c600ef70b
matthieugi <matthieugirard@outlook.com>
2021-03-22 11:33:32 +0000

    ajout réponses 404 si pas de données remontées depuis les bindings Azure Functions

--- matt/challenge3/GetRating/index.js
+++ matt/challenge3/GetRating/index.js
@@ -1,5 +1,11 @@
 module.exports = async function (context) {
 
+    if(context.bindings.rating === undefined) {
+        return {
+            status: 404
+        }
+    }
+
     return {
         body: context.bindings.rating
     }
--- matt/challenge3/GetRatings/index.js
+++ matt/challenge3/GetRatings/index.js
@@ -1,4 +1,10 @@
 module.exports = async function (context, req) {
+    if(context.bindings.ratings === undefined) {
+        return {
+            status: 404
+        }
+    }
+
     return {
         body: context.bindings.ratings
     }
