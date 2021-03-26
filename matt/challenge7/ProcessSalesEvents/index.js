7ee65df8e27aa5bf297f898a181cfafb8c355663
matthieugi <matthieugirard@outlook.com>
2021-03-26 14:21:43 +0000

    sync challenge 8

--- matt/challenge7/ProcessSalesEvents/index.js
+++ matt/challenge7/ProcessSalesEvents/index.js
@@ -5,19 +5,19 @@ const config = {
     key: process.env.cosmos_accountkey,
     databaseId: "BFYOC",
     containerId: "POSsales",
-    partitionKey: { kind: "Hash", paths: ["/productName"] }
-  };
+    partitionKey: { kind: "Hash", paths: ["/locationPostcode"] }
+};
 
 const client = new CosmosClient(config);
 const container = client.database(config.databaseId).container(config.containerId);
 
-module.exports = async function (context, eventHubMessages) {  
-    eventHubMessages.forEach(async (posSalesevent, index) => {
-        try {
-            await Promise.all(posSalesevent.details.map((item) => container.items.create(item)));
-        }
-        catch (Error) {
-            context.log('Error while processing event : ' + Error);
-        }
-    });
+module.exports = async function (context, eventHubMessages) {
+    try {
+        await Promise.all(eventHubMessages.map((item) => container.items.create(item)));
+    }
+    catch (Error) {
+        context.log('Error while processing event : ' + Error);
+    }
+
+
 };
