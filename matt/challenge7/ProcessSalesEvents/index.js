c7fd3c314ef2006e71ae62e6184674eab7a2c357
matthieugi <matthieugirard@outlook.com>
2021-03-29 07:40:58 +0000

    adapt challenge 7 with Bindings

--- matt/challenge7/ProcessSalesEvents/index.js
+++ matt/challenge7/ProcessSalesEvents/index.js
@@ -1,23 +1,24 @@
-const CosmosClient = require("@azure/cosmos").CosmosClient;
+// const CosmosClient = require("@azure/cosmos").CosmosClient;
 
-const config = {
-    endpoint: process.env.cosmos_endpoint,
-    key: process.env.cosmos_accountkey,
-    databaseId: "BFYOC",
-    containerId: "POSsales",
-    partitionKey: { kind: "Hash", paths: ["/locationPostcode"] }
-};
+// const config = {
+//     endpoint: process.env.cosmos_endpoint,
+//     key: process.env.cosmos_accountkey,
+//     databaseId: "BFYOC",
+//     containerId: "POSsales",
+//     partitionKey: { kind: "Hash", paths: ["/locationPostcode"] }
+// };
 
-const client = new CosmosClient(config);
-const container = client.database(config.databaseId).container(config.containerId);
+// const client = new CosmosClient(config);
+// const container = client.database(config.databaseId).container(config.containerId);
 
 module.exports = async function (context, eventHubMessages) {
-    try {
-        await Promise.all(eventHubMessages.map((item) => container.items.create(item)));
-    }
-    catch (Error) {
-        context.log('Error while processing event : ' + Error);
-    }
+    // try {
+    //     await Promise.all(eventHubMessages.map((item) => container.items.create(item)));
+    // }
+    // catch (Error) {
+    //     context.log('Error while processing event : ' + Error);
+    // }
 
+    context.bindings.sales = eventHubMessages;
 
 };
